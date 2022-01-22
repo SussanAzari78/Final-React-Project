4caab60ca19e5adda709bf236ff2b6d0073c87c4
sussan azari <95274243+SussanAzari78@users.noreply.github.com>
2022-01-22 07:19:50 +0330

    Add files via upload

---
+++ src/components/fallBox/index.js
@@ -0,0 +1,42 @@
+import FaalApi from "../api/FaalApi"; 
+import css from "./fallBox.module.css"
+function FaalBox(){
+    const {titleNumber , poem , Interpretation , voice } = FaalApi();
+    return(
+        <div>
+            <div className={css.top}>
+            <div className={css.poemContainer}>
+                    <p className={css.title}>{titleNumber}:</p>
+                    <div className={css.poem}>
+                        {poem.map((couplet, coupletIndex) => {
+                            return (
+                                <div
+                                    key={`couplet-${coupletIndex}`}
+                                    className={css.couplet}
+                                >
+                                    {couplet.map((verse, verseIndex) => {
+                                        return (
+                                            <div
+                                                key={`verse-${coupletIndex}-${verseIndex}`}
+                                                className={css.verse}
+                                            >
+                                                {verse}
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            );
+                        })}
+                    </div>{" "}
+                    <audio src={voice} controls></audio>
+        </div>
+            </div>
+            <div className={css.bottom}>
+                <p>ای صاحب فال:</p>
+                {Interpretation}
+            </div>
+
+        </div>
+    );
+}
+export default FaalBox;
