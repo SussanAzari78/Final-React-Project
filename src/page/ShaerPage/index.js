4d2fcd27ab3b0b1b15720ed16d8fc1b9ece933a3
sussan azari <95274243+SussanAzari78@users.noreply.github.com>
2022-01-22 07:25:00 +0330

    Add files via upload

---
+++ src/page/ShaerPage/index.js
@@ -0,0 +1,27 @@
+import css from "./shaerPage.module.css"
+import image4 from "../../assets/image4.png";
+import image1 from "../../assets/image1.png";
+import refreshimg from "../../assets/refresh.png";
+import ShaerApi from "../../components/api/shaerApi";
+import { useParams } from "react-router-dom";
+import PoemBox from "../../components/poemBox";
+// import Sher from "../../components/sher";
+function ShaerPage() {
+    const { id } = useParams();
+    const { name, refresh, image, title, poem,info,} = ShaerApi(id);
+    return (
+<div className={css.backgorund}>
+    <img style={{position: "absolute"}} src={image4} alt=""/>
+<div className={css.leftYellow}>
+<PoemBox title={title} image={image} poem={poem} />
+<img className={css.refreshing} src={refreshimg}  alt="" onClick={refresh}
+/>
+</div>
+<div className={css.rightBlue}>
+{info}
+</div>
+</div>
+        );
+}
+
+export default ShaerPage;
